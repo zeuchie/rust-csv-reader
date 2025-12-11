bbe77d3ee7da29bb32c8679f730e4206b011009f
zeuchie <48141433+zeuchie@users.noreply.github.com>
2025-12-11 10:22:41 -0500

    Implement file reading in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::fs;
-use std::io;
+use std::io::prelude::*;
 
 // Useful resource for learning rust standard library: https://docs.rs/rustc-std-workspace-std/latest/std/
 
 fn main() {
-    println!("Hello, world!");
+    // Create output.txt file
+    fs::write("output.txt", "Hello, world!").unwrap();
+
+    // Open output.txt and save to file variable, create string, read file to string
+    let mut file = fs::File::open("output.txt").unwrap();
+    let mut s = String::new(); //
+    file.read_to_string(&mut s).unwrap();
+
+    // Split string s at whitespace, print each word on new line with debug formatting
+    s.split_whitespace().for_each(|word| println!("{:?}", word));
 }
